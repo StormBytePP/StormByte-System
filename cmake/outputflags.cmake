2d0da9538681da1f8dc73c7ea1f6f3abe45cb948
David C. Manuelda <StormByte@gmail.com>
2026-08-31 00:50:14 +0200

    Removed Project top level flags display

--- cmake/outputflags.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "C Flags:         ${CMAKE_C_FLAGS}")
-message(STATUS "CXX Flags:       ${CMAKE_CXX_FLAGS}")
-message(STATUS "Exe Linker:      ${CMAKE_EXE_LINKER_FLAGS}")
-message(STATUS "Shared Linker:   ${CMAKE_SHARED_LINKER_FLAGS}")
