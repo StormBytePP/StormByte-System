50642ca5c450c8a07f30378b93ce504ca8297492
David C. Manuelda <StormByte@gmail.com>
2026-09-04 23:48:06 +0200

    docs: system pipe internals

--- lib/private/StormByte/system/pipe.hxx
+++ lib/private/StormByte/system/pipe.hxx
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte-System.
- *
- * StormByte-System is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte-System is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte-System. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte-System.
+*
+* StormByte-System is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte-System is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte-System. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
 
@@ -31,81 +31,81 @@
 #endif
 
 /**
- * @namespace System
- * @brief System utilities: processes, pipes, environment variables.
+ * @brief System module of the StormByte suite.
  */
 namespace StormByte::System {
 	/**
 	 * @class Pipe
 	 * @brief Cross-platform anonymous pipe for process IPC.
 	 *
-	 * UNIX: pipe(2)/pipe2; Windows: CreatePipe. Move-only.
+	 * UNIX: pipe(2)/pipe2. Windows: CreatePipe. Move-only.
 	 * @note On UNIX, SIGPIPE is ignored process-wide once (first Pipe construction).
 	 */
 	class STORMBYTE_SYSTEM_PRIVATE Pipe {
 		public:
 			/**
-			 * Maximum bytes per read operation (4 MiB).
+			 * @brief Maximum bytes per read (4 MiB).
 			 */
 			static constexpr const size_t MAX_READ_BYTES = 4 * 1024 * 1024;
 
 			/**
-			 * Creates a new pipe pair.
+			 * @brief Create a new pipe pair.
 			 */
 			Pipe();
 
 			/**
-			 * Copy constructor (deleted).
+			 * @brief Copy constructor (deleted).
 			 */
 			Pipe(const Pipe&) = delete;
 
 			/**
-			 * Move constructor.
+			 * @brief Move constructor.
 			 */
 			Pipe(Pipe&&) = default;
 
 			/**
-			 * Copy assignment (deleted).
+			 * @brief Copy assignment (deleted).
 			 */
 			Pipe& operator=(const Pipe&) = delete;
 
 			/**
-			 * Move assignment.
+			 * @brief Move assignment.
 			 */
 			Pipe& operator=(Pipe&&) = default;
 
 			/**
-			 * Closes both ends.
+			 * @brief Close both ends.
 			 */
 			~Pipe() noexcept;
 
 			#ifdef UNIX
 			/**
-			 * Dup2 read end onto @p fd.
+			 * @brief Dup2 read end onto @p fd.
 			 * @param fd Destination file descriptor.
 			 */
 			void BindRead(int fd) noexcept;
 
 			/**
-			 * Dup2 write end onto @p fd.
+			 * @brief Dup2 write end onto @p fd.
 			 * @param fd Destination file descriptor.
 			 */
 			void BindWrite(int fd) noexcept;
 
 			/**
-			 * Writes a string to the write end.
+			 * @brief Write a string to the write end.
 			 * @param str Data.
 			 * @return Bytes written.
 			 */
 			ssize_t Write(const std::string& str);
 
 			/**
-			 * @return true if the write end is no longer writable (HUP/ERR).
+			 * @brief Whether the write end is no longer writable (HUP/ERR).
+			 * @return true if so.
 			 */
 			bool WriteEOF() const;
 
 			/**
-			 * Reads into @p buffer up to @p size bytes.
+			 * @brief Read into @p buffer up to @p size bytes.
 			 * @param buffer Destination.
 			 * @param size Max bytes.
 			 * @return Bytes read.
@@ -113,43 +113,46 @@ namespace StormByte::System {
 			ssize_t Read(std::vector<char>& buffer, ssize_t size) const;
 
 			/**
-			 * @return true if the read end reports HUP/ERR.
+			 * @brief Whether the read end reports HUP/ERR.
+			 * @return true if so.
 			 */
 			bool ReadEOF() const;
 			#else
 			/**
-			 * Sets handle information on the read end.
+			 * @brief Set handle information on the read end.
 			 * @param mask Mask.
 			 * @param flags Flags.
 			 */
 			void ReadHandleInformation(DWORD mask, DWORD flags);
 
 			/**
-			 * Sets handle information on the write end.
+			 * @brief Set handle information on the write end.
 			 * @param mask Mask.
 			 * @param flags Flags.
 			 */
 			void WriteHandleInformation(DWORD mask, DWORD flags);
 
 			/**
-			 * @return Read HANDLE.
+			 * @brief Read HANDLE.
+			 * @return Handle.
 			 */
 			HANDLE ReadHandle() const;
 
 			/**
-			 * @return Write HANDLE.
+			 * @brief Write HANDLE.
+			 * @return Handle.
 			 */
 			HANDLE WriteHandle() const;
 
 			/**
-			 * Writes a string to the write end.
+			 * @brief Write a string to the write end.
 			 * @param str Data.
 			 * @return Bytes written.
 			 */
 			DWORD Write(const std::string& str);
 
 			/**
-			 * Reads into @p buffer up to @p size bytes.
+			 * @brief Read into @p buffer up to @p size bytes.
 			 * @param buffer Destination.
 			 * @param size Max bytes.
 			 * @return Bytes read.
@@ -158,31 +161,31 @@ namespace StormByte::System {
 			#endif
 
 			/**
-			 * Writes @p str in chunks until complete or peer closes.
+			 * @brief Write @p str in chunks until complete or peer closes.
 			 * @param str Data (moved). Empty string succeeds immediately.
 			 * @return true if all data was written.
 			 */
 			bool WriteAtomic(std::string&& str);
 
 			/**
-			 * Closes the read end.
+			 * @brief Close the read end.
 			 */
 			void CloseRead() noexcept;
 
 			/**
-			 * Closes the write end.
+			 * @brief Close the write end.
 			 */
 			void CloseWrite() noexcept;
 
 			/**
-			 * Writes @p str via Write().
+			 * @brief Write @p str via Write().
 			 * @param str Data.
 			 * @return *this.
 			 */
 			Pipe& operator<<(const std::string& str);
 
 			/**
-			 * Reads until EOF into @p str.
+			 * @brief Read until EOF into @p str.
 			 * @param str Destination.
 			 * @return Reference to @p str.
 			 */
@@ -198,26 +201,26 @@ namespace StormByte::System {
 
 			#ifdef UNIX
 			/**
-			 * Dup2 and close source.
+			 * @brief Dup2 and close source.
 			 * @param src Source fd (set to -1).
 			 * @param dst Destination fd.
 			 */
 			void Bind(int& src, int dst) noexcept;
 
 			/**
-			 * Closes @p fd if open.
+			 * @brief Close @p fd if open.
 			 * @param fd File descriptor.
 			 */
 			void Close(int& fd) noexcept;
 			#else
 			/**
-			 * Closes @p handle if valid.
+			 * @brief Close @p handle if valid.
 			 * @param handle Handle.
 			 */
 			void Close(HANDLE& handle) noexcept;
 
 			/**
-			 * SetHandleInformation wrapper.
+			 * @brief SetHandleInformation wrapper.
 			 * @param handle Handle.
 			 * @param mask Mask.
 			 * @param flags Flags.
