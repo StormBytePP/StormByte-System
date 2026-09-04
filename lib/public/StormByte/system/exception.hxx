6cd451c551b3aca04d930cbc994a79790c90faf6
David C. Manuelda <StormByte@gmail.com>
2026-09-04 23:48:13 +0200

    headers: make headers uniform

--- lib/public/StormByte/system/exception.hxx
+++ lib/public/StormByte/system/exception.hxx
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
-*
-* This file is part of StormByte-System.
-*
-* StormByte-System is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License version 3
-* or later, as published by the Free Software Foundation.
-*
-* StormByte-System is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with StormByte-System. If not, see
-* <https://www.gnu.org/licenses/lgpl-3.0.html>.
-*/
+ * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+ *
+ * This file is part of StormByte-System.
+ *
+ * StormByte-System is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * or later, as published by the Free Software Foundation.
+ *
+ * StormByte-System is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with StormByte-System. If not, see
+ * <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
 
 #pragma once
 
